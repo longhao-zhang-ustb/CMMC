057aef35c816575f4b37546d5247c6f77065155b
Longhao Zhang <zlh_ustb2022@163.com>
2026-03-11 15:49:28 +0800

    Enhance evaluate function with RoBERTa and metrics
    Added support for optional RoBERTa model and confusion matrix plotting.

--- evaluate.py
+++ evaluate.py
@@ -6,22 +6,15 @@ current_folder = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, current_folder)
 import torch
 from sklearn.metrics import precision_recall_fscore_support
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 from data import Dataset, BatchWrapper
 from model.net import Net
 import utils.tool as tool
 from prepare_data.rel2json import get_single_evaluate
+from sklearn.metrics import confusion_matrix
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', default='./data/', help="Directory containing the dataset")
-parser.add_argument('--embedding_pkl_path', default='./data/word_embedding', help="Path to word vecfile.")
-parser.add_argument('--model_dir', default='experiments/base_model', help="Directory containing params.json")
-parser.add_argument('--bert', default=False, help=" use Bert or wordembedding")
-parser.add_argument('--gpu', default=True, help="use GPU")
-parser.add_argument('--restore_file', default=None,
-                    help="Optional, name of the file in --model_dir containing weights to reload before training")
-
-def evaluate(model, test_data, metric_labels, mode='Train'):
+def evaluate(model, test_data, metric_labels, mode='Train', args=None, data_type='re_traced'):
     """Evaluate the model on `num_steps` batches."""
     # set model to evaluation mode
     model.eval()
@@ -33,9 +26,14 @@ def evaluate(model, test_data, metric_labels, mode='Train'):
     # compute metrics over the dataset
     for i, batch_data in enumerate(test_data):
         # fetch the next evaluation batch
-        words, pos1s, lens, pos2s, labels = batch_data
-        # compute model output
-        outputs = model(words, pos1s, pos2s)
+        if args.roberta:
+            words, pos1s, lens, pos2s, labels, attention_mask = batch_data
+            # compute model output
+            outputs = model(words, pos1s, pos2s, attention_mask)
+        else:
+            words, pos1s, lens, pos2s, labels = batch_data
+            # compute model output
+            outputs = model(words, pos1s, pos2s)
         # if outputs.shape[0] != labels.shape[0]:
         #     outputs = outputs[:labels.shape[0]]
         loss = model.loss(outputs, labels)
@@ -47,6 +45,7 @@ def evaluate(model, test_data, metric_labels, mode='Train'):
 
     # Calculate precision, recall and F1 for all relation categories micro==>macro
     # 暂时修改为micro-averge记录结果
+    p_r_f1_all = None
     if mode == 'Train':
         p_r_f1_s = precision_recall_fscore_support(target_labels, output_labels, labels=metric_labels, average='macro', zero_division=1)
         precision, recall, f1score, support = precision_recall_fscore_support(target_labels, output_labels, labels=metric_labels, average=None, zero_division=1)
@@ -57,10 +56,22 @@ def evaluate(model, test_data, metric_labels, mode='Train'):
         }
     # 暂时修改为micro-average
     elif mode == 'Test':
+        # 绘制混淆矩阵
+        # print(metric_labels)
+        # cm = confusion_matrix(target_labels, output_labels, labels=metric_labels)
+        # plt.figure(figsize=(10,7))
+        # sns.heatmap(cm, annot=True, fmt="d", cmap='Blues')  # fmt="d"表示整数格式，cmap选择颜色映射
+        # plt.ylabel('Actual label')
+        # plt.xlabel('Predicted label')
+        # plt.savefig(r'20260218_experimental_data\fig_save\confusion_matrix.png', dpi=1000)
+        # 打印错误的样本
+        # for i in range(len(target_labels)):
+        #     if target_labels[i] != output_labels[i]:
+        #         print(f"Sample {i}: True label = {target_labels[i]}, Predicted label = {output_labels[i]}")
         p_r_f1_s = precision_recall_fscore_support(target_labels, output_labels, labels=metric_labels, average='macro', zero_division=1)
         precision, recall, f1score, support = precision_recall_fscore_support(target_labels, output_labels, labels=metric_labels, average=None, zero_division=1)
         # 更换数据集时，这里的指标需要修改 semeval_18class、semeval_9class、i2b2、re_traced
-        p_r_f1_all = get_single_evaluate(precision=precision, recall=recall, f1score=f1score, data_type='re_traced')
+        p_r_f1_all = get_single_evaluate(precision=precision, recall=recall, f1score=f1score, data_type=data_type)
         logging.info(p_r_f1_all)
         p_r_f1 = {
             'precison': p_r_f1_s[0] * 100,
@@ -69,4 +80,4 @@ def evaluate(model, test_data, metric_labels, mode='Train'):
             'loss': loss_avg()
         }
     
-    return p_r_f1
+    return p_r_f1 if mode == 'Train' else (p_r_f1, p_r_f1_all)
